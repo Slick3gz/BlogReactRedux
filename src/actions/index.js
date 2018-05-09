50175cd423d1b1f3dc9fdd793ec748975604167c
Store Room <udchstore@ud.local>
2018-05-09 14:52:45 -0400

    added New Post, Show Post pages.  Completed form validation.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,8 @@
 import axios from 'axios'
 
 export const FETCH_POSTS = "FETCH_POSTS";
+export const CREATE_POST = "CREATE_POST";
+export const FETCH_POST = "FETCH_POST";
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=mushroom33';
@@ -10,4 +12,23 @@ export function fetchPosts() {
         type: FETCH_POSTS,
         payload: request
     };
+}
+
+export function createPost(values, callback) {
+    const request = axios.post(`${ROOT_URL}/posts${API_KEY}`, values)
+    .then(() => callback());
+
+    return {
+        type: CREATE_POST,
+        payload: request
+    }
+}
+
+export function fetchPost(id) {
+    const request = axios.get(`${ROOT_URL}/post${id}${API_KEY}`)
+    
+    return {
+        type: FETCH_POST,
+        palyload: request
+    }
 }
